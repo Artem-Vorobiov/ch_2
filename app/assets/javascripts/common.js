ab2760a28577c399ec0c5b940fa14811ae197ddd
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-08-20 19:47:23 +0300

    bags, but jQuery works...

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -28,6 +28,7 @@ $(function() {
 	
 });
 
+
 $(window).load(function() {
 
 	$(".loader_inner").fadeOut();
@@ -35,8 +36,34 @@ $(window).load(function() {
 
 });
 
-
-function lrt(){
-	alert('Look here...');
+/*
+function validation(){
+	var email = document.getElementById('contact_email').value;
+	var regVEmail =  /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i;
+	var result = email.search(regVEmail);
+	//alert(result);
+	if (result == "-1"){
+		alert("Error");
+	}
+	else alert(result);
 }
 
+*/
+
+$(document).ready(function() {
+    $('#contact_email').blur(function() {
+        if($(this).val() != '') {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if(pattern.test($(this).val())){
+                $(this).css({'border' : '1px solid #569b44'});
+                $('#valid').text('Верно');
+            } else {
+                $(this).css({'border' : '1px solid #ff0000'});
+                $('#valid').text('Не верно');
+            }
+        } else {
+            $(this).css({'border' : '1px solid #ff0000'});
+            $('#valid').text('Поле email не должно быть пустым');
+        }
+    });
+});
